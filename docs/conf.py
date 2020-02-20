76811917376b1f51b4674e63a2fb6258a1b3f3ec
Cristic <CristicMay@gmail.com>
2020-02-20 14:01:53 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -156,7 +156,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'WeEvent.tex', 'WeEvent Documentation',
-     'cristic', 'manual'),
+     'cristic', 'manual',True),
 ]
 
 
